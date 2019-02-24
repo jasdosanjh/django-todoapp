817892041f939e9e8db898df4f997957720da11c
jazdosanjh <jas@dosanjh.co.uk>
2019-02-24 11:48:08 +0000

    Created templates for the app

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request): 
+    return render(request, 'index.html', {})
+
+def about(request):
+    context = {'first_name': 'Gurpreet', 'last_name': 'Dosanjh'}
+    return render(request, 'about.html', context)
